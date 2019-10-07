6c0351da0a9b2d2ac64d68ea89c37952c6ac54e9
CalebJEJ <cjj226@nau.edu>
2019-10-06 22:31:37 -0700

    Added Menu and Tweening
    Created a menu screen for selecting levels and add in tweening for the how to play screen and menu screen transitions

--- Project_2_Puzzle/gameV2.js
+++ Project_2_Puzzle/gameV2.js
@@ -27,13 +27,19 @@ stage.addChild(startSprite);
 //begin animation
 animate();
 
+//set up variables for later sprites
+var backgroundSprite;
+var backgroundSprite2;
+var howToPlayTextSprite;
+var menuTextSprite;
+var levelZeroSprite;
+
+
 function startGame()
 {
 	//remove all children from the stage
 	stage.removeChildren();
-	//load in the "how to play" screen
-	var howToScreenSprite;
-	var howToScreenSprite2;
+	//load in the "how to play" screen sprite sheet
 	PIXI.Loader.shared.add('resources/waterAssets.json').load(setup);
 	
 	function setup() {
@@ -46,39 +52,60 @@ function startGame()
 		}
 
 		// create our animated sprite with the array
-		howToScreenSprite = new PIXI.AnimatedSprite(waterFrames);
+		backgroundSprite = new PIXI.AnimatedSprite(waterFrames);
 		
-		howToScreenSprite.anchor.set(0.5);
+		backgroundSprite.anchor.set(0.5);
 
-		howToScreenSprite.position.x = 160;
-		howToScreenSprite.position.y = 160;
+		backgroundSprite.position.x = 160;
+		backgroundSprite.position.y = 160;
 		
-		howToScreenSprite.animationSpeed = 0.01;
+		backgroundSprite.animationSpeed = 0.01;
 		
-		howToScreenSprite.gotoAndPlay(0);
+		backgroundSprite.gotoAndPlay(0);
 
 		//set the screens to go to the menu on click
-		howToScreenSprite.interactive = true;
-		howToScreenSprite.on('mousedown', loadMenu);
+		backgroundSprite.interactive = true;
+		backgroundSprite.on('mousedown', loadMenu);
 		
-		stage.addChild(howToScreenSprite);
+		stage.addChild(backgroundSprite);
 		
-		//second half of the how to screen
+		//second half of the how to screen background
 		
-		howToScreenSprite2 = new PIXI.AnimatedSprite(waterFrames);
+		backgroundSprite2 = new PIXI.AnimatedSprite(waterFrames);
 		
-		howToScreenSprite2.position.x = 480;
-		howToScreenSprite2.position.y = 160;
+		backgroundSprite2.position.x = 480;
+		backgroundSprite2.position.y = 160;
 		
-		howToScreenSprite2.anchor.set(0.5);
+		backgroundSprite2.anchor.set(0.5);
 		
-		howToScreenSprite2.animationSpeed = 0.01;
+		backgroundSprite2.animationSpeed = 0.01;
 		
-		howToScreenSprite2.gotoAndPlay(0);
+		backgroundSprite2.gotoAndPlay(0);
 		
-		stage.addChild(howToScreenSprite2);
+		stage.addChild(backgroundSprite2);
 		
-		animate();
+		
+		//load in the text of how to play on top
+		
+		var howToPlayText = PIXI.Texture.from("resources/How_To_Play_Text.png");
+
+		howToPlayTextSprite = new PIXI.Sprite(howToPlayText);
+
+		howToPlayTextSprite.anchor.x = 0.5;
+		howToPlayTextSprite.anchor.y = 0.5;
+		
+		howToPlayTextSprite.position.x = -320;
+		howToPlayTextSprite.position.y = 160;
+		
+		createjs.Tween.get(howToPlayTextSprite.position).to({x: 320, y: 160}, 2000, createjs.Ease.bounceOut);
+
+		
+		//set the text to go to the menu on click as well just in case
+		howToPlayTextSprite.interactive = true;
+		howToPlayTextSprite.on('mousedown', loadMenu);
+		
+		stage.addChild(howToPlayTextSprite);
+		
 	}
 
 	
@@ -87,20 +114,80 @@ function startGame()
 
 function loadMenu()
 {
-	//remove all children from the stage
-	//load in the menus background
+	//remove all uneeded children from the stage
+		//use a tween to have the text leave
+	createjs.Tween.get(howToPlayTextSprite.position).to({x: 320, y: -320}, 2000, createjs.Ease.bounceOut);
+	//turn off interactivity with background
+	backgroundSprite.interactive = false;
+	
+	//load in the text for the menu
+	var menuText = PIXI.Texture.from("resources/Menu_text.png");
+
+	menuTextSprite = new PIXI.Sprite(menuText);
+
+	menuTextSprite.anchor.x = 0.5;
+	menuTextSprite.anchor.y = 0.5;
+
+	menuTextSprite.position.x = 320;
+	menuTextSprite.position.y = 480;
+	
+	createjs.Tween.get(menuTextSprite.position).to({x: 320, y: 160}, 3000, createjs.Ease.circOut);
+	
+	//once the menu is in postion get rid of the offscreen how to text
+	if(menuTextSprite.position.y == 160)
+	{
+		stage.removeChild(howToPlayTextSprite);
+	}
+
+	stage.addChild(menuTextSprite);
 	
 	//load in the sprites for the levels
+
+    //Level Zero sprite
+	var levelZero = PIXI.Texture.from("resources/Level0Menu.png");
+
+	levelZeroSprite = new PIXI.Sprite(levelZero);
+
+	levelZeroSprite.anchor.x = 0.5;
+	levelZeroSprite.anchor.y = 0.5;
+
+	levelZeroSprite.position.x = 160;
+	levelZeroSprite.position.y = 480;
 	
-	//set the level sprites to load their corresponding level on click
+	createjs.Tween.get(levelZeroSprite.position).to({x: 160, y: 160}, 2000, createjs.Ease.circOut);
+
+	//set the level icon to go to the level on click
+	howToPlayTextSprite.interactive = true;
+	howToPlayTextSprite.on('mousedown', loadLevel0);
+
+	stage.addChild(levelZeroSprite);
+	
+	
+	//Level One sprite
+	var levelOne = PIXI.Texture.from("resources/Level1Menu.png");
+
+	levelOneSprite = new PIXI.Sprite(levelOne);
+
+	levelOneSprite.anchor.x = 0.5;
+	levelOneSprite.anchor.y = 0.5;
+
+	levelOneSprite.position.x = 320;
+	levelOneSprite.position.y = 480;
+	
+	createjs.Tween.get(levelOneSprite.position).to({x: 160, y: 160}, 2500, createjs.Ease.circOut);
+
+	//set the level icon to go to the level on click
+	howToPlayTextSprite.interactive = true;
+	howToPlayTextSprite.on('mousedown', loadLevel0);
+
+	stage.addChild(levelOneSprite);
+	
 }
 
-//this function loads up a game level matching the ID code it was given
-function loadLevel(levelID)
+//this function loads up level 0
+function loadLevel0()
 {
-	//remove all children from the stage
-	//check the ID code
-		//load the level based on that ID code
+	//remove all non background children from the stage
 		//create a keboard listener for WASD keys for character movement
 		//the listen will also listen for the O key which will open an options menu
 			//Option menu conatian the following:
@@ -109,11 +196,28 @@ function loadLevel(levelID)
 				//A go back to menu button
 			//levels have the following:
 				//A background sprite
-				//Several sprites for the ice blocks
-					//Ice blocks are removed once the character leaves its space
-				//A main character sprite
-					//can only onto spaces where an Ice Block is
-			//Levels end when the alloted moves for the level are used up resulting in a winning screen
+				//Several sprites for the puzzle blocks
+					//One puzzle black is hidden the rest are jumbled
+				//A completed version of the puzzle
+			//Levels end when each of the jumbled puzzle's pieces postioning matches the complete ones
+}
+
+//this function loads up level 1
+function loadLevel1()
+{
+	//remove all non background children from the stage
+		//create a keboard listener for WASD keys for character movement
+		//the listen will also listen for the O key which will open an options menu
+			//Option menu conatian the following:
+				//A button for turning music off
+				//A reset the level button
+				//A go back to menu button
+			//levels have the following:
+				//A background sprite
+				//Several sprites for the puzzle blocks
+					//One puzzle black is hidden the rest are jumbled
+				//A completed version of the puzzle
+			//Levels end when each of the jumbled puzzle's pieces postioning matches the complete ones
 }
 
 function animate()
